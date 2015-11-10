60bfa6fb2f821a70c8bf560f339a640bc45eae29
lithiumdenis <lithiumdenis@gmail.com>
2015-11-10 19:06:38 +0300

    first usability multithreaded version of server

--- Server/server.c
+++ Server/server.c
@@ -1,73 +1,105 @@
-/*
-	C socket server example
-*/
+#include <stdio.h>      /* for printf() and fprintf() */
+#include <sys/socket.h> /* for socket(), bind(), and connect() */
+#include <arpa/inet.h>  /* for sockaddr_in and inet_ntoa() */
+#include <stdlib.h>     // exit()
+#include <string.h>     // memset()
+#include <unistd.h>     // close() и write()
+#include <pthread.h>
 
-#include<stdio.h>
-#include<string.h>	//strlen
-#include<sys/socket.h>
-#include<arpa/inet.h>	//inet_addr
-#include<unistd.h>	//write
-
-int main(int argc , char *argv[])
+#define N_THREADS 10 //Число рабочих потоков
+#define MAXPENDING 5 // Выдаётся времени на запрос соединения
+//Структура с параметрами, которую передаём в каждый поток
+struct thread_param
 {
-	int socket_desc , client_sock , c , read_size;
-	struct sockaddr_in server , client;
-	char client_message[2000];
-	
-	//Create socket
-	socket_desc = socket(AF_INET , SOCK_STREAM , 0);
-	if (socket_desc == -1)
+    int serverSocket;
+};
+//Все наши потоки
+pthread_t thread_pool[N_THREADS];
+
+//На сервере возникла ошибка и он прекращает работу, выводя её
+static void die(const char *message)
+{
+    perror(message);
+    exit(1); 
+}
+
+//Создание сокет сервера
+static int createServerSocket(unsigned short port)
+{
+    int servSock;
+    struct sockaddr_in servAddr;
+
+    //Создание сокета для входящих подключений
+    if ((servSock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0)
+        die("socket() failed");
+      
+   //Задаём начальные параметры
+    memset(&servAddr, 0, sizeof(servAddr));       // Заполняем структуру нулями
+    servAddr.sin_family = AF_INET;                // Семейство адресов
+    servAddr.sin_addr.s_addr = htonl(INADDR_ANY); // IP-адрес
+    servAddr.sin_port = htons(port);              // Порт
+
+    //Назначаем номер порта клиентскому сокету
+    if (bind(servSock, (struct sockaddr *)&servAddr, sizeof(servAddr)) < 0)
+        die("bind() failed");
+
+    //Отмечаем сколько сокет ждет запрос со стороны клиента
+    if (listen(servSock, MAXPENDING) < 0)
+        die("listen() failed");
+
+    return servSock;
+}
+
+void* threadMain(void *tparam)
+{
+    //Приняли параметры в поток
+    struct thread_param *param = (struct thread_param *)tparam;
+    int servSock = param -> serverSocket;
+    
+    //Адрес клиента
+    struct sockaddr_in clntAddr;
+    char client_message[2000];
+    
+    int read_size;
+    
+    for (;;) 
+    {
+        //Ожидание коннекта с клиентом
+
+        unsigned int clntLen = sizeof(clntAddr); 
+        int clntSock = accept(servSock, (struct sockaddr *)&clntAddr, &clntLen);
+        if (clntSock < 0)
+            die("accept() failed");
+
+        //Получаем сообщение от клиента
+	while( (read_size = recv(clntSock , client_message , 2000 , 0)) > 0 )
 	{
-		printf("Could not create socket");
+		//Отправляем обратно клиенту
+		write(clntSock , client_message , strlen(client_message));
 	}
-	puts("Socket created");
-	
-	//Prepare the sockaddr_in structure
-	server.sin_family = AF_INET;
-	server.sin_addr.s_addr = INADDR_ANY;
-	server.sin_port = htons( 8888 );
-	
-	//Bind
-	if( bind(socket_desc,(struct sockaddr *)&server , sizeof(server)) < 0)
-	{
-		//print the error message
-		perror("bind failed. Error");
-		return 1;
-	}
-	puts("bind done");
-	
-	//Listen
-	listen(socket_desc , 3);
-	
-	//Accept and incoming connection
-	puts("Waiting for incoming connections...");
-	c = sizeof(struct sockaddr_in);
-	
-	//accept connection from an incoming client
-	client_sock = accept(socket_desc, (struct sockaddr *)&client, (socklen_t*)&c);
-	if (client_sock < 0)
-	{
-		perror("accept failed");
-		return 1;
-	}
-	puts("Connection accepted");
-	
-	//Receive a message from client
-	while( (read_size = recv(client_sock , client_message , 2000 , 0)) > 0 )
-	{
-		//Send the message back to client
-		write(client_sock , client_message , strlen(client_message));
-	}
-	
-	if(read_size == 0)
-	{
-		puts("Client disconnected");
-		fflush(stdout);
-	}
-	else if(read_size == -1)
-	{
-		perror("recv failed");
-	}
-	
-	return 0;
+
+   }
+   pthread_exit(NULL);
+}
+
+int main(int argc, char *argv[])
+{
+    //Задали порт
+    unsigned short servPort = 8888;
+    //Создали сокет
+    int servSock = createServerSocket(servPort);
+    int i;
+    struct thread_param param;
+    param.serverSocket = servSock;
+    //Содаём N_THREADS потоков
+    for(i=0;i<N_THREADS;i++)
+    {
+        pthread_create(&thread_pool[i],NULL,&threadMain,(void*)&param);
+    }
+    //Ожидаем завершения всех N_THREADS потоков
+    for(i=0;i<N_THREADS;i++)
+    {
+        pthread_join(thread_pool[i],NULL);
+    }
+    return 0;
 }
